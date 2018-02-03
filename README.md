1637414e0ff061856e7a23288cdcc504495dbf27
dannyrc100 <dannyrc100@hotmail.com>
2018-02-03 08:41:55 -0500

    Survey setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FriendFinder
+
+Paulinho
+http://as01.epimg.net/futbol/imagenes/2017/08/14/primera/1502708979_420176_1502709040_noticia_normal.jpg
--- app/data/friends.js
+++ app/data/friends.js
@@ -3,30 +3,30 @@ var friends = [{
     photo: "https://www.101greatgoals.com/wp-content/uploads/2017/09/GettyImages-846142820.jpg",
     scores: [
       5,
+      4,
+      4,
+      3,
       5,
+      3,
       5,
+      2,
       5,
-      5,
-      5,
-      5,
-      5,
-      5,
-      5
+      1
     ]
   },
   {
     name: "Luis Suarez",
     photo: "https://provenquality.cdn.speedyrails.net/wp-content/uploads/2015/03/Luis-Suarez-wears-jersey-number-9-at-Barcelona.jpg",
     scores: [
+      5,
+      4,
       1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
+      3,
+      3,
+      4,
+      5,
+      3,
+      2,
       1
     ]
   },
